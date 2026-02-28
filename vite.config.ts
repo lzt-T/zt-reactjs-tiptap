720872e07a2857adc20ed4932073871a7f447a19
林志腾 <linzhiteng2022@163.com>
2026-03-01 01:08:05 +0800

    feat: 更新版本0.1.10，优化react-dom构建配置

--- vite.config.ts
+++ vite.config.ts
@@ -20,12 +20,13 @@ export default defineConfig({
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ["react", "react-dom", "react/jsx-runtime"],
+      external: ["react", /^react-dom/, "react/jsx-runtime"],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
+          "react-dom/client": "ReactDOM",
           "react/jsx-runtime": "react/jsx-runtime",
         },
       },
